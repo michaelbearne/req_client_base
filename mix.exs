1098a4b0c0f9232778fdbf05784c92cdf36fab4c
Michael Bearne <michaelbearne@gmail.com>
2025-05-22 12:33:07 +0100

    add license and config for publish to hex

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule ReqClientBase.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/dualohq/req_client_base"
+
   def project do
     [
       app: :req_client_base,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      docs: docs(),
+      deps: deps(),
+      package: package(),
+      description: description()
     ]
   end
 
@@ -24,7 +30,32 @@ defmodule ReqClientBase.MixProject do
       {:req, "~> 0.5"},
       {:req_fuse, "~> 0.3"},
       {:req_telemetry, "~> 0.1"},
-      {:opentelemetry_req, "~> 1.0"}
+      {:opentelemetry_req, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Utility base macro for building HTTP clients with Req."
+  end
+
+  # https://hexdocs.pm/hex/Mix.Tasks.Hex.Build.html#module-package-configuration
+  defp package do
+    [
+      name: "req_client_base",
+      maintainers: ["Michael Bearne"],
+      links: %{"GitHub" => @source_url},
+      licenses: ["SEE LICENSE IN LICENSE"],
+      organization: "dualo"
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
